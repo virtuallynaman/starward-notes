bb1c3910fb91994ae790dbe68e89b69edfa94304
virtuallynaman <jhanaman786@gmail.com>
2025-03-24 16:33:00 +0530

    Update notes context

--- frontend/src/NotesProvider.jsx
+++ frontend/src/NotesProvider.jsx
@@ -1,28 +1,74 @@
+import { useAuth } from "./AuthProvider";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { NotesContext } from "./NotesContext";
+import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "sonner";
 
-function NotesProvider({ children }) {
+const NotesContext = createContext();
+
+export function NotesProvider({ children }) {
+    const { user } = useAuth();
     const [notes, setNotes] = useState([]);
+    const [viewStyle, setViewStyle] = useState("grid-view");
+    const [noteType, setNoteType] = useState("all")
+    const [sortOrder, setSortOrder] = useState("DESC");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
-    //Get notes
+    // Check for saved sort order and viewstyle
     useEffect(() => {
+        const savedSortOrder = localStorage.getItem("sortOrder");
+        const savedViewStyle = localStorage.getItem("viewStyle");
+
+        if (savedSortOrder) {
+            setSortOrder(savedSortOrder);
+        }
+
+        if (savedViewStyle) {
+            setViewStyle(savedViewStyle);
+        }
+    }, []);
+
+    // Save sort order and view style to localstorage
+    useEffect(() => {
+        localStorage.setItem("sortOrder", sortOrder);
+        localStorage.setItem("viewStyle", viewStyle);
+    }, [sortOrder, viewStyle]);
+
+    useEffect(() => {
+        setSearchTerm("");
+        setIsLoading(true);
+
         const fetchNotes = async () => {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/notes`);
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/notes?noteType=${noteType}&sortOrder=${sortOrder}`, { headers: { "Authorization": `Bearer ${user.accessToken}` } });
                 setNotes(response.data);
             } catch (err) {
                 console.error("Error fetching notes:", err);
+                toast.error(err.message);
+            } finally {
+                setIsLoading(false);
             }
-        };
-        fetchNotes();
-    }, []);
+        }
+        setTimeout(() => {
+            fetchNotes();
+        }, 1000);
+    }, [user, noteType, sortOrder]);
+
+    // Filter notes
+    const filteredNotes = searchTerm
+        ? notes.filter(note =>
+            note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            note.body.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        : [];
 
     return (
-        <NotesContext.Provider value={{notes, setNotes}}>
+        <NotesContext.Provider value={{ notes, setNotes, viewStyle, setViewStyle, noteType, setNoteType, sortOrder, setSortOrder, searchTerm, setSearchTerm, filteredNotes, isLoading }}>
             {children}
         </NotesContext.Provider>
     );
 };
 
-export default NotesProvider;
+export function useNotes() {
+    return useContext(NotesContext);
+}
