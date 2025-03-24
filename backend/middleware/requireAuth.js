15b842fc781b310dceb9eb312eaf74452a125b9b
virtuallynaman <jhanaman786@gmail.com>
2025-03-24 06:56:51 +0530

    Add jwt verification

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const requireAuth = async (req, res, next) => {
+
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(400).json({ error: "Authorization token required" });
+    }
+
+    const token = authorization.split(" ")[1];
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+
+        req.user = payload;
+        next();
+    } catch (err) {
+        console.error("Error verifying the token", err);
+        return res.status(401).json({ error: "Request is not authorized" })
+    }
+
+}
+
+// module.exports = requireAuth
+export default requireAuth
