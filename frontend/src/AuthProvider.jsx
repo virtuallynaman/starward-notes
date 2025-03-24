981ebc23dcb9828d31127092252f247540e27c2b
virtuallynaman <jhanaman786@gmail.com>
2025-03-24 06:57:50 +0530

    Add global auth context

---
+++ frontend/src/AuthProvider.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState();
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+        const isUser = JSON.parse(localStorage.getItem('user'));
+        if (isUser) {
+            setUser(isUser);
+        }
+    }, [])
+    
+    const login = (responseData) => {
+        const userData = { email: responseData.email, accessToken: responseData.accessToken };
+        localStorage.setItem('user', JSON.stringify(userData));
+        setUser(userData);
+        setIsAuth(true);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        setUser(null);
+        setIsAuth(false);
+    }
+
+    return (
+        <AuthContext.Provider value={{ isAuth, user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
