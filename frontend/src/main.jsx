02a3372a6270b500f809fa5c55e6bc5b33ba64c3
virtuallynaman <jhanaman786@gmail.com>
2025-03-24 06:58:31 +0530

    Add authprovider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './AuthProvider.jsx'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    // <StrictMode>
+    <AuthProvider>
+        <Toaster
+            toastOptions={{
+                style: {
+                    background: '#202124',
+                },
+            }}
+        />
+        <App />
+    </AuthProvider >
+    // </StrictMode>
 )
