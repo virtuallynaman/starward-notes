b7924e9cd68fd6ef3ac74d23f26ee82f81327c67
virtuallynaman <jhanaman786@gmail.com>
2025-05-03 23:09:31 +0530

    Customise toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,14 +7,24 @@ import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
     // <StrictMode>
+
+    // Provides auth context to the entire app
     <AuthProvider>
+
+        {/* Toaster for toast notifications */}
         <Toaster
             toastOptions={{
                 style: {
                     background: '#202124',
+                    minWidth: '200',
+                    maxWidth: '400px',
+                    width: 'fit-content',
+                    wordBreak: 'break-word'
                 },
             }}
+            visibleToasts={2}
         />
+        {/* Main app component */}
         <App />
     </AuthProvider >
     // </StrictMode>
